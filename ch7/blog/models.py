7f53e346bd9c497f838946acdb6060b564da47bb
sujikim <sujikim0924@gmail.com>
2016-11-15 17:21:11 +0900

    Ch7 완료 - Blog 앱 확장 (Tag 달기)

--- ch7/blog/models.py
+++ ch7/blog/models.py
@@ -3,6 +3,7 @@ from django.utils.encoding import python_2_unicode_compatible
 
 from django.db import models
 from django.core.urlresolvers import reverse
+from tagging.fields import TagField
 
 # Create your models here.
 
@@ -14,6 +15,7 @@ class Post(models.Model):
     content = models.TextField('CONTENT')
     create_date = models.DateTimeField('Create Date', auto_now_add=True)
     modify_date = models.DateTimeField('Modify Date', auto_now=True)
+    tag = TagField()
 
     class Meta:
         verbose_name = 'post'
--- ch7/blog/views.py
+++ ch7/blog/views.py
@@ -1,10 +1,16 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
 
 from blog.models import Post
+from tagging.models import Tag, TaggedItem
+from tagging.views import TaggedObjectList
 
 # Create your views here.
+
+#--- TemplateView
+class TagTV(TemplateView) :
+    template_name = 'tagging/tagging_cloud.html'
 
 #--- ListView
 class PostLV(ListView) :
@@ -12,6 +18,10 @@ class PostLV(ListView) :
     template_name = 'blog/post_all.html'
     context_object_name = 'posts'
     paginate_by = 2
+
+class PostTOL(TaggedObjectList) :
+    model = Post
+    template_name = 'tagging/tagging_post_list.html'
 
 #--- DetailView
 class PostDV(DetailView) :
