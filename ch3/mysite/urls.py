cf4bc5c3a46bb37d2f5e76dfdfb93645fc23ae5f
sujikim <sujikim0924@gmail.com>
2016-11-15 15:16:13 +0900

    Ch3 완료

--- ch3/mysite/urls.py
+++ ch3/mysite/urls.py
@@ -14,15 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-from bookmark.views import BookmarkLV, BookmarkDV
+#from bookmark.views import BookmarkLV, BookmarkDV
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
+    url(r'^blog/', include('blog.urls', namespace='blog')),
 
     # Class-based views for Bookmark app
-    url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
-    url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
+    #url(r'^bookmark/$', BookmarkLV.as_view(), name='index'),
+    #url(r'^bookmark/(?P<pk>\d+)/$', BookmarkDV.as_view(), name='detail'),
 ]
