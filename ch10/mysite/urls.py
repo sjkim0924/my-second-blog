7199f2c7fc4a300e729dffcc8fd29671279d45ba
sujikim <sujikim0924@gmail.com>
2016-11-16 18:03:25 +0900

    Ch10 완료 - 실전 프로그램 개발 (Photo 앱)

--- ch10/mysite/urls.py
+++ ch10/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 from mysite.views import HomeView
 
@@ -25,4 +27,6 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
     url(r'^blog/', include('blog.urls', namespace='blog')),
-]
+    url(r'^photo/', include('photo.urls', namespace='photo')),
+
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
