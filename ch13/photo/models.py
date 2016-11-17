334540d53016a8f13ec67e0ecf6ee293be38f42a
sujikim <sujikim0924@gmail.com>
2016-11-17 12:08:14 +0900

    Ch13 완료 - 실전 프로그램 개발 (콘텐츠 편집 기능-Photo 앱)

--- ch13/photo/models.py
+++ ch13/photo/models.py
@@ -6,12 +6,15 @@ from django.core.urlresolvers import reverse
 
 from photo.fields import ThumbnailImageField
 
+from django.contrib.auth.models import User
+
 # Create your models here.
 
 @python_2_unicode_compatible
 class Album(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['name']
@@ -29,6 +32,7 @@ class Photo(models.Model):
     image = ThumbnailImageField(upload_to='photo/%Y/%m')
     description = models.TextField('Photo Description', blank=True)
     upload_date = models.DateTimeField('Upload Date', auto_now_add=True)
+    owner = models.ForeignKey(User, null=True)
         
     class Meta:
         ordering = ['title']
