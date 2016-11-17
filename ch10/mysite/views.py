84ba819b743b256175b62b2254a415c09084d8ee
sujikim <sujikim0924@gmail.com>
2016-11-17 11:18:18 +0900

    Ch11 완료 - 실전 프로그램 개발 (인증 기능)

--- ch10/mysite/views.py
+++
@@ -1,9 +0,0 @@
-from django.views.generic.base import TemplateView
-
-# Create your views here.
-
-#--- TemplateView
-class HomeView(TemplateView):
-    template_name = 'home.html'
-
-
--- ch11/mysite/urls.py
+++ ch11/mysite/urls.py
@@ -10,19 +10,23 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Add an import:  from blog import urls as blog_urls
+    2. Import the include() function: from django.conf.urls import url, include
+    3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 
 from mysite.views import HomeView
+from mysite.views import UserCreateView, UserCreateDoneTV
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^accounts/register/$', UserCreateView.as_view(), name='register'),
+    url(r'^accounts/register/done/$', UserCreateDoneTV.as_view(), name='register_done'),
 
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^bookmark/', include('bookmark.urls', namespace='bookmark')),
@@ -30,3 +34,4 @@ urlpatterns = [
     url(r'^photo/', include('photo.urls', namespace='photo')),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
